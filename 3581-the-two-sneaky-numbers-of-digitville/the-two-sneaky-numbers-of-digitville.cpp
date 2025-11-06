9951fbef290c3374f844a34d7024a4f4aeaacfac
Kushagra srivastava <122214707+marvillage@users.noreply.github.com>
2025-11-06 12:14:28 +0530

    Time: 0 ms (100.00%) | Memory: 25.2 MB (85.73%) - LeetSync

---
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> getSneakyNumbers(vector<int>& nums) {
+        vector<int> ans;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[i] == nums[j]) {
+                    ans.push_back(nums[i]);
+                    if (ans.size() == 2) return ans; // since only two sneaky numbers
+                }
+            }
+        }
+        
+        return ans; // in case constraints are different
+    }
+};
